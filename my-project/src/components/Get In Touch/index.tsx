7390987db21d59c254dbac44f6ed51712ccac3d9
Fatya <NO>
2024-11-14 16:43:34 +0400

    bug

--- my-project/src/components/Get In Touch/index.tsx
+++ my-project/src/components/Get In Touch/index.tsx
@@ -1,21 +1,29 @@
+import GetInTouchContact from "../GetInTouchContact";
 import GetInTouchForm from "../GetInTouchForm";
-
+import fData from "../../form.json"
 
 
 const GetInTouch = () => {
     return (
 
-        <div className="flex items-center w-full pt-10 gap-20 h-auto bg-gradient-to-br from-blue-500 to-indigo-300 text-white">
-            <div className="flex flex-col gap-5 pl-44 mt-11">
-                <h1 className="text-4xl font-bold uppercase">get in touch</h1>
-                <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
-                
-                <GetInTouchForm/>
+        <div className="flex flex-col  w-full pt-10 pl-44 pb-10 gap-10 h-auto bg-gradient-to-br from-blue-500 to-indigo-300 text-white">
+
+            <h1 className="text-4xl font-bold uppercase">get in touch</h1>
+
+            <div className="flex w-[970px] justify-between">
+                <p className="text-lg opacity-55">Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+                <h2 className="text-lg">Contact-info</h2>
+
             </div>
-            <div className="flex flex-col gap-5 px-1 mt-11">
-                d
+
+            <div className="flex">
+
+                <GetInTouchForm />
+
+                {fData.map(fData => <GetInTouchContact key={fData.id} address={fData.address} mobile={fData.mobile} email={fData.email} />)}
+
             </div>
-            
+
         </div>
 
     )
